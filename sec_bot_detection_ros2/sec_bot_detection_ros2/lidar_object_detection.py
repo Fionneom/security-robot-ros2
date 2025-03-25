1152d5c299c4a889f07ace1d042d85a42eda6689
Fionn <fionneomuiri@gmail.com>
2025-03-25 16:12:37 +0000

    Can detect objects

--- sec_bot_detection_ros2/sec_bot_detection_ros2/lidar_object_detection.py
+++ sec_bot_detection_ros2/sec_bot_detection_ros2/lidar_object_detection.py
@@ -3,27 +3,48 @@ from rclpy.node import Node
 
 import sensor_msgs.msg
 
+import numpy as np
+
 class LidarDetection(Node):
     def __init__(self):
         super().__init__('Lidar_Object_Detection')
 
-        # timer_period = 0.1  # seconds
-        # self.timer = self.create_timer(timer_period, self.timer_callback)
-
         self.lidar_subscription = self.create_subscription(sensor_msgs.msg.LaserScan, 'scan', self.lidar_callback, 10) 
 
-        self.old_ranges = []
-        self.filter_size = 5
-    # def timer_callback(self):
-    #     pass
+        self.range_history = []
+        self.old_average = []
+        self.filter_size = 7
+        self.cluster_size = 3
 
     def lidar_callback(self, msg):
         ranges = msg.ranges
 
-        self.old_ranges.append(ranges)
+        self.range_history.append(ranges)
 
-        if len(self.old_ranges) > self.filter_size:
-            self.old_ranges.pop(0)
+        if len(self.range_history) > self.filter_size:
+            self.range_history.pop(0)
+
+        average = []
+
+        for i in range(len(self.range_history[0])):
+            row = []
+            for j in range(len(self.range_history)):
+                row.append(round(self.range_history[j][i], 1))
+            average.append(np.median(row))
+
+        
+        if self.old_average != []:
+            for i in range(len(average)):
+                if(self.old_average[i] - average[i] > 0.2):
+                    if all(self.old_average[j] - average[j] > 0.2 for j in range(i - self.cluster_size, i + self.cluster_size)):
+                        self.get_logger().info("Object at: " + str(i))
+                        i += self.cluster_size
+                        
+
+        self.old_average = average
+
+
+        
 
         
 
